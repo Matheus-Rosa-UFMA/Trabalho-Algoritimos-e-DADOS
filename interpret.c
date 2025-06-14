5b5fe61eaece0448932daa6c7c4357819d7cb2ee
MATHEUS-ROSA-UFMA <matheus.rosa@discente.ufma.br>
2025-06-14 14:03:25 -0300

    INTERPRET FINISH

--- interpret.c
+++ interpret.c
@@ -1,14 +1,52 @@
+// interpret.c
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <ctype.h>
 #include "interpret.h"
+#include "stack.h"
 
-void interpret (const char *source) {
+static Stack* pilha = NULL;
+
+void set_stack(Stack* s) {
+    pilha = s;
+}
+
+void interpret(const char *source) {
     char op[10];
     char arg[10];
 
-    sscanf (source, "%s%s", op, arg);
-    printf("operação: %s\n", op);
-    printf("argumento: %s\n",  arg);
+    int n = sscanf(source, "%s %s", op, arg);
+
+    if (strcmp(op, "exit") == 0) {
+        exit(0);
+    } else if (strcmp(op, "push") == 0 && n == 2) {
+        int value = atoi(arg);
+        stack_push(pilha, value);
+    } else if (strcmp(op, "add") == 0) {
+        int b = stack_pop(pilha);
+        int a = stack_pop(pilha);
+        stack_push(pilha, a + b);
+    } else if (strcmp(op, "sub") == 0) {
+        int b = stack_pop(pilha);
+        int a = stack_pop(pilha);
+        stack_push(pilha, b - a);
+    } else if (strcmp(op, "mul") == 0) {
+        int b = stack_pop(pilha);
+        int a = stack_pop(pilha);
+        stack_push(pilha, a * b);
+    } else if (strcmp(op, "div") == 0) {
+        int b = stack_pop(pilha);
+        int a = stack_pop(pilha);
+        if (b == 0) {
+            printf("Erro: divisao por zero.\n");
+        } else {
+            stack_push(pilha, a / b);
+        }
+    } else if (strcmp(op, "print") == 0) {
+        int value = stack_pop(pilha);
+        printf("%d\n", value);
+    } else {
+        printf("Comando inválido: %s\n", op);
+    }
 }
--- main.c
+++ main.c
@@ -1,26 +1,19 @@
 #include <stdio.h>
-
 #include "interpret.h"
+#include "stack.h"
 
 static void repl()
 {
-  char line[1024];
-  for (;;)
-  {
-    printf("> ");
-
-    if (!fgets(line, sizeof(line), stdin))
-    {
-      printf("\n");
-      break;
+    char line[1024];
+    for (;;) {
+        printf("> ");
+        if (!fgets(line, sizeof(line), stdin)) {
+            printf("\n");
+            break;
+        }
+        interpret(line);
     }
-
-    interpret(line);
-  }
 }
-
-
-
 
 int main () {
 
